6c0639321ef2d4fc146ecf2fd790b50aded60334
JayPatel <jay.patel1@gameskraft.com>
2024-05-07 20:18:05 +0530

    delete all files

--- src/pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-function HomePage(){
-    return (
-        <>
-            Home Page
-        </>
-    );
-}
-
-export default HomePage;
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { appReducer } from "./reducers/app-reducer";
-import { legacy_createStore as createStore ,combineReducers } from "redux";
-
-const rootReducer = combineReducers({
-  app: appReducer,
-});
-
-export const store = createStore(rootReducer);
-store.subscribe(() => console.log(store.getState()));
