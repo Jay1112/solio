f831183eb6a3d92111061d8db121b115636432f4
Jay Patel <jay.patel1@gameskraft.com>
2024-07-24 22:59:51 +0530

    user dashboard

--- src/components/Dashboard/DashboardForm.jsx
+++ src/components/Dashboard/DashboardForm.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Input from "../ui/Input";
 import TextArea from "../ui/TextArea";
+import Button from '../ui/Button';
 
 function DashboardForm() {
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(e.target[0].value);
+  }
+
   return (
-    <form className="flex w-full flex-col">
+    <form onSubmit={handleSubmit} className="flex w-full flex-col">
       <div className="flex w-full flex-col md:flex-row">
         <div className="flex-1 pr-0 md:pr-2">
           <Input
@@ -36,6 +43,18 @@ function DashboardForm() {
         containerStyle="w-full mt-4"
         className="w-full primary-input min-h-[250px] md:min-h-[100px] p-2.5 my-2 rounded-sm"
       />
+      <div className="mt-3">
+        <Button
+          buttonStyle="inline-block primary-button px-3 py-[6px] text-lg rounded-sm"
+          type="submit"
+          // disabled={loading}
+        >
+          <span className="font-poppins">
+            {/* {loading ? "Loading..." : "Save"} */}
+            Save
+          </span>
+        </Button>
+      </div>
     </form>
   );
 }
